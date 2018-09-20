be27bca90c4a763160b54df4fbda6f307756ae9d
Aby <danielaparralesd.m@gmail.com>
2018-09-20 11:44:20 -0500

    Read the file and store it in a linear array

--- lab1.9/multiplier.c
+++ lab1.9/multiplier.c
@@ -4,7 +4,7 @@
 
 long NUM_BUFFERS = 5;      //Número de hilos ejecutándose de una vez
 
-/*void print_mat(long *mat){
+/* void print_mat(long *mat){
   int i,j;
 
    for(i = 0; i < 2000; i++){
@@ -16,7 +16,7 @@ long NUM_BUFFERS = 5;      //Número de hilos ejecutándose de una vez
 /*
 Reads matrix file and returns an long type array with content of matrix.
  */
-void readMatrix(char *filename){    
+void readMatrix_b(char *filename){    
 //long * readMatrix(char *filename){    
   long i;
   long j;
@@ -48,12 +48,57 @@ void readMatrix(char *filename){
   //returns *mat;
 }
 
-/*void test_threads(){
+/*
+Reads matrix file and returns an long type array with content of matrix.
+ */
+void readMatrix_a(char *filename){    
+//long* readMatrix_a(char *filename){    
+  long i=0;   //index of the matrix
 
-}*/
+  //creates an array that will be the returned as the matrix readed
+  long* mat=malloc(4000000*sizeof(long)); 
+  
+  //initialize the matrix
+  /*for(i=0;i<4000000;++i)
+    *mat=&malloc(sizeof(long));
+  */
+  //read the file given by filename string
+  FILE *file;
+  file=fopen(filename, "r");
+
+  //testing...
+  // fscanf(file, "%ld", &mat[i]);
+  // printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
+  // i++;
+  // fscanf(file, "%ld", &mat[i]);
+  // printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
+
+  for(i = 0; i < 4000000; i++){
+    fscanf(file, "%ld", &mat[i]);
+    printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
+  }
+
+ /* for(i = 0; i < 10; i++){
+    //for(j = 0; j < 4; j++){
+      //Use ld for longs
+      if (!fscanf(file, "%ld", mat[i]))     //checks that the file is't empty
+        printf("El archivo está vacio\n");
+        break;                              //In such case it breaks
+      printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
+    //}
+  }*/
+
+  fclose(file);
+  //returns *mat;
+}
+
+
+void test_threads(){
+
+}
 
 
 int main(){
     //print_mat(readMatrix("matA.dat"));
-    readMatrix("matA.dat");
+    readMatrix_a("matA.dat");
 }
