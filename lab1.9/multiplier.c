c6b784dc7e7f4825a61ae17f2f762c024fcb1912
Aby <danielaparralesd.m@gmail.com>
2018-09-20 17:07:24 -0500

    Method getColumn ready, just missing the adjusts for a variable size

--- lab1.9/multiplier.c
+++ lab1.9/multiplier.c
@@ -4,92 +4,37 @@
 
 long NUM_BUFFERS = 5;      //Número de hilos ejecutándose de una vez
 
-/* void print_mat(long *mat){
-  int i,j;
+void print_mat(long *mat,int x){
+  int i;
 
-   for(i = 0; i < 2000; i++){
-    for(j = 0; j < 4; j++){
-      printf("%ld\n",mat[i][j]); //Use lf for longs
+   for(i = 0; i<(x); i++){
+     printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
     }
-}*/
-
-/*
-Reads matrix file and returns an long type array with content of matrix.
- */
-void readMatrix_b(char *filename){    
-//long * readMatrix(char *filename){    
-  long i;
-  long j;
-
-  /*matrix*/
-  /*Use double , you have floating numbers not long*/
-
- long** mat=malloc(4000000*sizeof(long*)); 
-  //long** mat=malloc(4000000*sizeof(long*)); 
-
-  for(i=0;i<4000000;++i)
-    mat[i]=malloc(4*sizeof(long));
-
-
-  FILE *file;
-  file=fopen(filename, "r");
-
-  for(i = 0; i < 2000; i++){
-    for(j = 0; j < 4; j++){
-      //Use ld for longs
-      if (!fscanf(file, "%ld", &mat[i][j])) 
-        break;
-      // mat[i][j] -= '0'; 
-      printf("%ld\n",mat[i][j]); //Use lf for longs
-    }
-
-  }
-  fclose(file);
-  //returns *mat;
 }
 
 /*
 Reads matrix file and returns an long type array with content of matrix.
- */
-void readMatrix_a(char *filename){    
-//long* readMatrix_a(char *filename){    
-  long i=0;   //index of the matrix
+*/
+//void readMatrix_a(char *filename){    
+long* readMatrix(char *filename){    
+  long i=0;                                         //index of the matrix
 
   //creates an array that will be the returned as the matrix readed
-  long* mat=malloc(4000000*sizeof(long)); 
-  
-  //initialize the matrix
-  /*for(i=0;i<4000000;++i)
-    *mat=&malloc(sizeof(long));
-  */
-  //read the file given by filename string
+  long* mat=malloc(9*sizeof(long)); 
+  //long* mat=malloc(4000000*sizeof(long)); 
+
   FILE *file;
   file=fopen(filename, "r");
 
-  //testing...
-  // fscanf(file, "%ld", &mat[i]);
-  // printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
-  // i++;
-  // fscanf(file, "%ld", &mat[i]);
-  // printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
-
-  for(i = 0; i < 4000000; i++){
+  for(i = 0; i < 9; i++){
+  //for(i = 0; i < 4000000; i++){
     fscanf(file, "%ld", &mat[i]);
-    printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
+    //printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
   }
 
- /* for(i = 0; i < 10; i++){
-    //for(j = 0; j < 4; j++){
-      //Use ld for longs
-      if (!fscanf(file, "%ld", mat[i]))     //checks that the file is't empty
-        printf("El archivo está vacio\n");
-        break;                              //In such case it breaks
-      printf("i= %d ::: %ld\n",i, mat[i]);               //prints the long found in the line
-    //}
-  }*/
 
-  fclose(file);
-  //returns *mat;
+  fclose(file); 
+  return mat;
 }
 
 
@@ -97,8 +42,33 @@ void test_threads(){
 
 }
 
+/*
+  Returns the specified array that represents a column in the matrix array.
+*/
+//void getColumn(int col, long *matrix){
+long * getColumn(int col, long *matrix){
+  int i;
+  long* column = malloc(3*sizeof(long)); 
+  //long* column = malloc(2000*sizeof(long)); 
+
+  // for(i=0;i<2000;i++) {
+  //   column[i]=matrix[i+(col*2000)];
+  //   printf("i= %d ::: %ld\n",i, column[i]);               //prints the long found in the line
+  // }
+
+  //testing
+  for(i=0;i<3;i++) {
+    column[i]=matrix[i+(col*3)];
+    //printf("i= %d ::: %ld\n",i, column[i]);               //prints the long found in the line
+  }
+  return column;
+}
+
 
 int main(){
-    //print_mat(readMatrix("matA.dat"));
-    readMatrix_a("matA.dat");
+    //print_mat_a(readMatrix_a("matA.dat"));
+    //readMatrix("matA.dat");
+    //getColumn(1,readMatrix("tst.dat"));
+    print_mat(getColumn(1,readMatrix("tst.dat")),3);
+
 }
