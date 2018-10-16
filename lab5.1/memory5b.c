41e6ed84a0bec1058cf174d544c3e46dd12daad5
Aby <danielaparralesd.m@gmail.com>
2018-10-16 01:15:30 -0500

    Lab finished

--- lab5.1/memory5b.c
+++ lab5.1/memory5b.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-int main(){
+
+int main() {
+
 	char z = *(const char *)0;
-	printf(“I read from location zero\n”);
+	printf("I read from location zero\n");
+
 	exit(EXIT_SUCCESS);
 }
